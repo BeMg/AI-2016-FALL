90778939303ac7194bb30c253209422e8e5f6de8
BeMg <gccbg04538@gmail.com>
2016-11-24 01:13:10 -0800

    add mark_fail

--- n-queen/main.cpp
+++ n-queen/main.cpp
@@ -8,6 +8,27 @@ struct table_for_dfs {
     int table[MAX_TABLE_SIZE][MAX_TABLE_SIZE];
 };
 
+table_for_dfs mark_fail(table_for_dfs curr, int x, int y) {
+    int size = curr.size;
+
+    for(int i=0; i<size; i++) {
+	curr.table[i][y] = -1;
+	curr.table[x][i] = -1;
+	if((x+i)<size && (y+i)<size)
+	    curr.table[x+i][y+i] = -1;
+	if((x-i)>=0 && (y-i)>=0)
+	    curr.table[x-i][y-i] = -1;
+	if((x+i)<size && (y-i)>=0)
+	    curr.table[x+i][y-i] = -1;
+	if((x-i)>=0 && (y+i)<size)
+	    curr.table[x-i][y+i] = -1;
+    }
+    curr.table[x][y] = 1;
+
+    return curr;
+    
+}
+
 int DFS(table_for_dfs curr, int now_row) {
     int size = curr.size;
 
@@ -15,19 +36,16 @@ int DFS(table_for_dfs curr, int now_row) {
 	return 1;
     }
 
-
     for(int i=0; i<size; i++) {
 
-	if(curr[now_row][i] == 0) {
+	if(curr.table[now_row][i] == 0) {
 	   table_for_dfs tmp = mark_fail(curr, now_row, i);
 	   int ret = DFS(tmp, now_row+1);
 	   if(ret) return 1;
 	}
     
     }
-
     return 0;
-
 }
 
 
