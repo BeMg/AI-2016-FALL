86634f930837d728649d756eb20dda2ce3dc7c8d
BeMg <gccbg04538@gmail.com>
2016-12-07 13:46:37 +0800

    just use svm

---
+++ ML/main.py
@@ -0,0 +1,25 @@
+from numpy import genfromtxt
+from sklearn import svm
+
+ALL_DATA = genfromtxt('TraData.csv', delimiter=',')
+
+
+train = ALL_DATA[0:1500, 0:57]
+train_ans = ALL_DATA[0:1500, 57:58]
+
+test = ALL_DATA[1500:3000, 0:57]
+test_ans = ALL_DATA[1500:3000, 57:58]
+
+clf = svm.SVC()
+clf.fit(test, test_ans)
+
+ans = clf.predict(train)
+
+cnt = 0
+
+for i in range(1500):
+    if(ans[i] == train_ans[i]):
+        cnt+=1;
+
+
+print(cnt/1500)
