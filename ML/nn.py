967e53c068d9f4e7f441df656076897973e567f1
BeMg <gccbg04538@gmail.com>
2016-12-11 16:02:58 +0800

    magic 94%

--- ML/nn.py
+++ ML/nn.py
@@ -3,12 +3,14 @@ from sklearn.neural_network import MLPClassifier
 
 DATA = numpy.genfromtxt('TraData2.csv', delimiter=',')
 
+idx = 0
+Max_value = 0
 
 test = DATA[2700:3000 , 0:57]
 test_ans = DATA[2700:3000 , 57:58]
 
-train = DATA[0:2700 , 0:57]
-train_ans = DATA[0:2700, 57:58]
+train = DATA[0:2396 , 0:57]
+train_ans = DATA[0:2396, 57:58]
 
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
                     hidden_layer_sizes=(4, 2), random_state=2, activation='tanh')
@@ -23,4 +25,4 @@ for i in range(300):
     if(test_ans[i] == ans[i]):
         cnt+=1
 
-print(int(cnt/300*100))
+print("{}%".format(int(cnt/300*100)))
